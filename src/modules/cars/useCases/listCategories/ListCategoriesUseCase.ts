0fc436491bb46d34178cae7bbae19cd097274100
Joao Junior <joaojunior346@gmail.com>
2021-06-15 07:51:24 -0300

    refatoramento das categorys

--- src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -1,4 +1,4 @@
-import { Category } from "../../model/Category";
+import { Category } from "../../entities/Category";
 import { ICateagoriesRepository } from "../../repositories/ICateagoriesRepository";
 
 class ListCategoriesUseCase {
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,17 @@
 import { Router } from "express";
 import multer from "multer";
 
-import { createCategoryControllers } from "../modules/cars/useCases/createCategory";
+import createCategoryControllers from "../modules/cars/useCases/createCategory";
 import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const CategoriesRoutes = Router();
-
 const upload = multer({
   dest: "./tmp",
 });
 
 CategoriesRoutes.post("/", (request, response) => {
-  return createCategoryControllers.handle(request, response);
+  return createCategoryControllers().handle(request, response);
 });
 
 CategoriesRoutes.get("/", (request, response) => {
